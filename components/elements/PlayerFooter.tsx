c199e0a604711b8364088a943e2aa3fbd3d15f34
elia <eliasidori@outlook.it>
2025-06-13 11:54:37 +0200

    Correzione di un errore di formattazione nel componente PlayerFooter, rimuovendo uno spazio extra nella dichiarazione delle proprietà. Miglioramento della leggibilità del codice.

--- components/elements/PlayerFooter.tsx
+++ components/elements/PlayerFooter.tsx
@@ -24,7 +24,7 @@ export default function PlayerFooter() {
         playQueue,
         currentTrackIndex,
         trackProgress,
-        hidePlayer, 
+        hidePlayer,  
         playNext, 
         playPrevious,
         playTracks,
